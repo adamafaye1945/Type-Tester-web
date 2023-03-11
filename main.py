a758e810e0d84ac4636e3bc4a84f0d03b7a24f65
adamafaye1945 <46406662+adamafaye1945@users.noreply.github.com>
2023-03-11 20:58:16 +0300

    ready

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import os
 app = Flask(__name__)
 db = SQLAlchemy()
 app.secret_key = os.environ.get('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user_database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 login_manager = LoginManager()
